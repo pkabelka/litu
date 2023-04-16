7b28c0e0c7c69f85e6fde37bab30f15965df6690
pkabelka <pkabelka@email.cz>
2023-04-16 20:55:44 +0200

    added deduplication of slice using set

---
+++ deduplicate.go
@@ -0,0 +1,6 @@
+package litu
+
+func DedupeSlice[T comparable](a []T) []T {
+    set := NewSetFromSlice(a)
+    return set.ToSlice()
+}
---
+++ deduplicate_test.go
@@ -0,0 +1,24 @@
+package litu
+
+import (
+	"testing"
+)
+
+func TestDedupeSlice(t *testing.T) {
+    x := []string{"a", "b", "c", "b", "c", "d"}
+
+    res := DedupeSlice(x)
+
+    expect := []string{"a", "b", "c", "d"}
+    for _, e := range expect {
+        occurences := 0
+        for _, r := range res {
+            if e == r {
+                occurences++
+            }
+        }
+        if occurences != 1 {
+            t.Errorf("res = %q, want %q", res, expect)
+        }
+    }
+}
