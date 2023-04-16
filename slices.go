3672e767c827230376aa0fa46a56bab8d97c48e1
pkabelka <pkabelka@email.cz>
2023-04-16 21:55:46 +0200

    moved Equal and EqualUnordered to slices.go

--- slices.go
+++ slices.go
@@ -29,3 +29,27 @@ func Reverse[T ~[]U, U any](a T) T {
     ReverseInplace(rev)
     return rev
 }
+
+func Equal[T comparable](a, b []T) bool {
+    if len(a) != len(b) {
+        return false
+    }
+    for i, e := range a {
+        if e != b[i] {
+            return false
+        }
+    }
+    return true
+}
+
+func EqualUnordered[T comparable](a, b []T) bool {
+    if len(a) != len(b) {
+        return false
+    }
+    for _, e := range a {
+        if !InSlice(b, e) {
+            return false
+        }
+    }
+    return true
+}
--- slices_test.go
+++ slices_test.go
@@ -82,3 +82,73 @@ func TestReverseInplace(t *testing.T) {
 		t.Errorf("res = %v, want %v", x, expect)
     }
 }
+
+func TestEqual(t *testing.T) {
+    a := []string{"a", "b", "c"}
+    b := []string{"a", "b", "c"}
+
+    res := Equal(a, b)
+
+    if res != true {
+		t.Errorf("res = %v, want %v", res, true)
+    }
+
+    b = []string{"a", "b", "d"}
+
+    res = Equal(a, b)
+
+    if res != false {
+		t.Errorf("res = %v, want %v", res, false)
+    }
+
+    b = []string{}
+
+    res = Equal(a, b)
+
+    if res != false {
+		t.Errorf("res = %v, want %v", res, false)
+    }
+
+    a = []string{}
+
+    res = Equal(a, b)
+
+    if res != true {
+		t.Errorf("res = %v, want %v", res, true)
+    }
+}
+
+func TestEqualUnordered(t *testing.T) {
+    a := []string{"a", "b", "c"}
+    b := []string{"b", "c", "a"}
+
+    res := EqualUnordered(a, b)
+
+    if res != true {
+		t.Errorf("res = %v, want %v", res, true)
+    }
+
+    b = []string{"a", "b", "d"}
+
+    res = EqualUnordered(a, b)
+
+    if res != false {
+		t.Errorf("res = %v, want %v", res, false)
+    }
+
+    b = []string{}
+
+    res = EqualUnordered(a, b)
+
+    if res != false {
+		t.Errorf("res = %v, want %v", res, false)
+    }
+
+    a = []string{}
+
+    res = EqualUnordered(a, b)
+
+    if res != true {
+		t.Errorf("res = %v, want %v", res, true)
+    }
+}
