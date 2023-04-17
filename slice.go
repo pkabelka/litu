edda727907f29b3b48049ec774b366c58b77514a
pkabelka <pkabelka@email.cz>
2023-04-17 19:40:09 +0200

    slice: simplified Equal

--- slice.go
+++ slice.go
@@ -36,8 +36,8 @@ func Equal[T comparable](a, b []T) bool {
 	if len(a) != len(b) {
 		return false
 	}
-	for i, e := range a {
-		if e != b[i] {
+	for i := range a {
+		if a[i] != b[i] {
 			return false
 		}
 	}
