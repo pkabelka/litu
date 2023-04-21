0601107dc5fd81465ac94b3210506391035eeb3d
pkabelka <pkabelka@email.cz>
2023-04-21 18:59:15 +0200

    deduplicate: optimized by looping only once

--- deduplicate.go
+++ deduplicate.go
@@ -1,6 +1,13 @@
 package litu
 
 func DedupeSlice[T comparable](a []T) []T {
-	set := NewSetFromSlice(a)
-	return set.ToSlice()
+	set := NewSet[T](len(a))
+	res := make([]T, 0, len(a))
+	for _, e := range a {
+		if !set.Contains(e) {
+			set.Add(e)
+			res = append(res, e)
+		}
+	}
+	return res
 }
